e66f776debcfc23da460112d648b0e0746fe9212
zadvajr <danielzadva@gmail.com>
2024-06-25 12:57:59 +0100

    minor fix on bmi category

--- scripts.js
+++ scripts.js
@@ -18,9 +18,9 @@ document
     let category = "";
     if (bmi < 18.5) {
       category = "Underweight";
-    } else if (bmi >= 18.5) {
+    } else if (bmi >= 18.5 && bmi < 24.9) {
       category = "Normal weight";
-    } else if (bmi >= 25 && bmi < 24.9) {
+    } else if (bmi >= 25 && bmi < 29.9) {
       category = "Overweight";
     } else {
       category = "Obesity";
